33728fdd9674a20f0040c15733f90ad0ffe9fbc6
unknown <dandarabiancas@gmail.com>
2023-07-11 22:42:34 -0300

    portifolio

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css';
+import Navbar from './Components/Navbar/index.jsx';
+function App() {
+  return (
+    <div className="App">
+     <Navbar />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Navbar/index.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import './styles.css'
+export default function Navbar() {
+    return (
+        <div>
+            <nav>
+            <div className="border">
+      <h1>Dandara</h1>
+      <ul>
+        <li>Sobre mim: </li>
+        <li>Meus projetos: </li>
+        <li>Contatos: </li>
+        <li>teste</li>
+        
+      </ul>
+    </div>
+            </nav>
+        </div>
+        );
+}

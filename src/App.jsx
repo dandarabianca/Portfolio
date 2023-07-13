1e90647983b0df72b64799c048e43c171a80d3d4
unknown <dandarabiancas@gmail.com>
2023-07-12 22:36:39 -0300

    Portifolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
 import Navbar from "./Components/Navbar/index.jsx";
+import Menu from "./Components/Menu/index";
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Menu />
     </div>
   );
 }
---
+++ src/Components/Menu/index.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./styles.css";
+
+export default function Menu(){
+   return(
+    <div className="Home-conteier">
+    <div className="intro-menu">
+          <h2>Óla me chamo</h2>
+          <h1>Dandara Bianca</h1>
+        </div>
+        <div className="styles-menu">
+
+        </div>
+        </div>
+   )
+}
